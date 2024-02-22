ee98cff5ace178b2b79521b251c57e7229f90cfe
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-02-22 12:00:49 +0100

    workflow user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Database\Factories\PackageFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,5 +25,16 @@ class DatabaseSeeder extends Seeder
 
         User::factory(3)->create();
         Package::factory(20)->create();
+
+        User::create([
+            'name' => 'Lazarre',
+            'lastname' => 'CHOUBIYI',
+            'email' => 'lynia-shop@gmail.com',
+            'country' => 'France',
+            'email_verified_at' => now(),
+            'password' => bcrypt('lynia@2024'),
+            'remember_token' => Str::random(10),
+            'role_id' => 1,
+        ]);
     }
 }
