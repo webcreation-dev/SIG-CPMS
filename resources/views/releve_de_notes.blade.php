72bc1afea4c038e08a9230a1510ad32679d12fbd
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-03-29 10:19:31 +0100

    maj functions

--- resources/views/releve_de_notes.blade.php
+++ resources/views/releve_de_notes.blade.php
@@ -140,7 +140,7 @@
                             <td style="text-align: center;" rowspan="{{$ecues_count}}" >{{$ue->credit}}</td>
                             <td>{{$ecue_first->name}}</td>
                             <td style="text-align: center;">{{$ecue_first->credit}}</td>
-                            <td><strong>
+                            <td style="text-align: center;"><strong>
                                 {{ rtrim(rtrim(number_format(is_null((App\Models\ElementTeachingUnit::getNote($ecue_first->id, $studentId)?->moy_catch_up)) ? (App\Models\ElementTeachingUnit::getNote($ecue_first->id, $studentId)?->moy_ecu) : (App\Models\ElementTeachingUnit::getNote($ecue_first->id, $studentId)?->moy_catch_up), 3, '.', ''), '0'), '.') }}
                             </strong></td>
                             <td style="text-align: center;" rowspan="{{$ecues_count}}"><strong>  {{ rtrim(rtrim(number_format($moy_ue, 3, '.', ''), '0'), '.') }} </strong></td>
