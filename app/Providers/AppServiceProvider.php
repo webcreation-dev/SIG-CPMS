7cf541115e5a23170ea976a9e55f4480671f6fc1
blezdubien <anatoblez@gmail.com>
2025-02-11 01:12:21 +0100

    fix bug deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
+        if (config('app.env') !== 'local') {
+            \URL::forceScheme('https');
+        }
     }
+
 }
